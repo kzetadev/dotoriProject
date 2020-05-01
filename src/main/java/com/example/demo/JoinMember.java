c64fa9bfff52fc98dfcc19a42c3f0656451e13ec
o_o3293@naver.com <o_o3293@naver.com>
2020-05-01 14:21:56 +0900

    -------홍수지-------------- pom.xml 시큐리티 주석 제거 member_infoController 생성 joinmember 생성 -------------------------

---
+++ src/main/java/com/example/demo/JoinMember.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@SpringBootApplication
+public class JoinMember { //회원가입 처리
+	//패스워드 암호화를 위한 객체 생성
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		System.out.println("----------패스워드 인코딩 객체 생성----------");
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		
+	
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(JoinMember.class, args);
+	}
+
+}
--- src/main/java/com/example/demo/controller/Member_InfoController.java
+++ src/main/java/com/example/demo/controller/Member_InfoController.java
@@ -1,5 +1,29 @@
 package com.example.demo.controller;
 
-public class Member_InfoController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.example.demo.dao.Member_InfoDao;
+
+@Controller
+public class Member_InfoController {
+	
+	@Autowired
+	private Member_InfoDao m_dao;
+
+	public void setM_dao(Member_InfoDao m_dao) {
+		this.m_dao = m_dao;
+	}
+	
+	@RequestMapping("/listMember_info")
+	public ModelAndView listMember_info() {
+		System.out.println("listMember_info 컨트롤러 동작");
+		ModelAndView m = new ModelAndView();
+		m.setViewName("listMember_info");
+		m.addObject("list", m_dao.login());
+		return m;
+	}
+	
 }
