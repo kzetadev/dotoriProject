c86f5f0037324d0160837714c925ff2f35e3a0b6
izzie1101 <izzie1101@daum.net>
2020-05-01 15:49:38 +0900

    Board_PostDao 수정

--- src/main/java/com/example/demo/dao/Board_PostDao.java
+++ src/main/java/com/example/demo/dao/Board_PostDao.java
@@ -9,8 +9,24 @@ import com.example.demo.vo.Board_PostVo;
 
 @Repository
 public class Board_PostDao {
+	
+	//커뮤니티 글 목록
 	public List<Board_PostVo> listBoard_Post(){
 		return Board_PostManager.listBoard_Post();
 	}
 	
+	//커뮤니티 글 등록
+	public int insertBoard_Post(Board_PostVo vo) {
+		return Board_PostManager.insertBoard_Post(vo);
+	}
+	
+	//커뮤니티 글 수정
+	public int updateBoard_Post(Board_PostVo vo) {
+		return Board_PostManager.updateBoard_Post(vo);
+	}
+	
+	//커뮤니티 글 삭제
+	public int deleteBoard_Post(Board_PostVo vo) {
+		return Board_PostManager.deleteBoard_Post(vo);
+	}
 }
