28d8f0cc833077b079f7851fdc5d29054b97630f
izzie1101 <izzie1101@daum.net>
2020-05-08 09:54:58 +0900

    //

--- src/main/java/com/example/demo/controller/Board_PostController.java
+++ src/main/java/com/example/demo/controller/Board_PostController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,6 +26,7 @@ public class Board_PostController {
 	//커뮤니티 글 목록
 	@RequestMapping("/listBoard_Post.do")
 	public ModelAndView listBoard_Post() {
+		
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("listBoard_Post");
 		mav.addObject("list", b_dao.listBoard_Post());
