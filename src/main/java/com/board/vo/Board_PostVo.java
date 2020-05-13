61dd1e97364537284ee8cbb82a41aed00d563ff7
Administrator <Administrator@siriushp>
2020-05-14 00:07:49 +0900

    board_post vo 수정

--- src/main/java/com/board/vo/Board_PostVo.java
+++ src/main/java/com/board/vo/Board_PostVo.java
@@ -15,7 +15,7 @@ public class Board_PostVo {
 	private int board_kinds;		// 게시판 분류(자유, 후기, 동행)
 	private String head_tag_name;	// 말머리명
 	private String board_title;		// 글제목
-	private int mem_nickname;		// 회원 닉네임
+	private String mem_nickname;		// 회원 닉네임
 	private Date board_date;		// 게시일
 	private int board_hit;			// 조회수
 }
