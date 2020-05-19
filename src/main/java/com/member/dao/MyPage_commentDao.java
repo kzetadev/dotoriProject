3d08229e5defcedb6d54f493c41ab85c9c20326d
o_o3293@naver.com <o_o3293@naver.com>
2020-05-19 16:54:27 +0900

    myPage 내가 쓴 글 - 홍수지

--- src/main/java/com/member/dao/MyPage_commentDao.java
+++ src/main/java/com/member/dao/MyPage_commentDao.java
@@ -5,8 +5,13 @@ import java.util.List;
 
 import com.member.vo.Member_InfoVo;
 import com.member.vo.MyPage_CommentVo;
+import com.member.vo.MyPage_PostVo;
 
 public interface MyPage_commentDao {
 
+	//내가 쓴 댓글 목록
 	public List<MyPage_CommentVo> list(int mem_no) throws Exception;
+	
+	//내가 쓴 글 목록
+	public List<MyPage_PostVo> list_post(int mem_no) throws Exception;
 }
--- src/main/java/com/member/service/MyPage_commentService.java
+++ src/main/java/com/member/service/MyPage_commentService.java
@@ -5,10 +5,14 @@ import java.util.Map;
 
 import com.member.vo.Member_InfoVo;
 import com.member.vo.MyPage_CommentVo;
+import com.member.vo.MyPage_PostVo;
 
 public interface MyPage_commentService {
 	//내가 쓴 댓글 목록
 	public List<MyPage_CommentVo> list(int mem_no) throws Exception;
+	
+	//내가 쓴 글 목록
+	public List<MyPage_PostVo> list_post(int mem_no) throws Exception;
 
 
 
